50eccad4a1ac4ee997dd1f4e29aef1cb46aa092f
NguyenTrongAnh <anhnguyen20040312@gmail.com>
2025-02-19 08:15:07 +0700

    commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const layout = () => {
+  return (
+    <html>
+      <body>
+        <div>layout1</div>
+      </body>
+    </html>
+  );
+};
+
+export default layout;
