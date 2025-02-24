91d0641c49dc9e0654bc01959a966ee4db2d088c
NguyenTrongAnh <anhnguyen20040312@gmail.com>
2025-02-25 03:10:19 +0700

    1

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <header></header>
+      <body></body>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
