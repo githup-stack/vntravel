0ad9d7fcddb4e2d8a935afe3a28fb794a26de42c
NguyenTrongAnh <anhnguyen20040312@gmail.com>
2025-02-28 08:55:41 +0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import React, { ReactNode } from "react";
 
 interface LayoutProps {
@@ -12,7 +14,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <main>
-      <header>{/* Header content here */}</header>
-      <section>{/* Main content here */}</section>
-      <footer>{/* Footer content here */}</footer>
-    </main>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
